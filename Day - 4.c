869bd92ce8a3773f5b37b099432dcdeb220435f6
VibhuttiiSharma <vibhutisharma021@gmail.com>
2024-10-01 21:36:16 +0530

    Add files via upload

---
+++ Day - 4.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main() {
+    int A, B, C, X;
+
+    // Read the input values
+    scanf("%d %d %d %d", &A, &B, &C, &X);
+
+    // Check if the given topic matches any of the prepared topics
+    if (X == A || X == B || X == C) {
+        printf("Yes\n");
+    } else {
+        printf("No\n");
+    }
+
+    return 0;
+}
+
