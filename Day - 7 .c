4031ef2d9dcaf294815b3dfa1ac1ccd27c208bf2
VibhuttiiSharma <vibhutisharma021@gmail.com>
2024-10-06 23:26:10 +0530

    Update Day - 7 .c

--- Day - 7 .c
+++ Day - 7 .c
@@ -2,17 +2,15 @@
 #include <stdio.h>
 
 int main() {
-    int T; // Number of test cases
+    int T; 
     scanf("%d", &T);
 
     while (T--) {
-        int X; // Time when Janmansh starts the assignments (1 PM to 9 PM)
+        int X; 
         scanf("%d", &X);
 
-        // Convert X (1 PM to 9 PM) into 24-hour format by adding 12
         int start_time = X + 12;
 
-        // Check if Janmansh can finish all assignments before or by 10 PM (22:00)
         if (start_time + 3 <= 22) {
             printf("Yes\n");
         } else {
