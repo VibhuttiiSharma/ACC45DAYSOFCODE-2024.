672e533d45fa15c941c56ac0ffc86f49ca00fd5a
VibhuttiiSharma <vibhutisharma021@gmail.com>
2024-10-06 23:22:43 +0530

    Update Day - 8.c

--- Day - 8.c
+++ Day - 8.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 
 int main() {
-    int T;  // Number of test cases
-    scanf("%d", &T);  // Reading the number of test cases
+    int T;  
+    scanf("%d", &T);  
 
     while (T--) {
-        int P, Q, R, S;  // Profits of companies A, B, C, and D
-        scanf("%d %d %d %d", &P, &Q, &R, &S);  // Reading profits
+        int P, Q, R, S;  
+        scanf("%d %d %d %d", &P, &Q, &R, &S);  
 
-        // Check for monopoly condition: one profit is strictly greater than the sum of the other three
         if (P > (Q + R + S) || Q > (P + R + S) || R > (P + Q + S) || S > (P + Q + R)) {
             printf("YES\n");
         } else {
