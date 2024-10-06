7aff863e416ab1b7c46dea5925443afcf68bffbd
VibhuttiiSharma <vibhutisharma021@gmail.com>
2024-10-06 23:24:45 +0530

    Update Day - 3.c

--- Day - 3.c
+++ Day - 3.c
@@ -2,16 +2,14 @@
 
 int main() {
     int T;
-    scanf("%d", &T);  // Read the number of test cases
+    scanf("%d", &T);  
 
     while (T--) {
         int X, Y;
-        scanf("%d %d", &X, &Y);  // Read X and Y for each test case
-
-        // Calculate total hours
+        scanf("%d %d", &X, &Y);  
+        
         int total_hours = (X * 4) + Y;
         
-        // Output the result for each test case
         printf("%d\n", total_hours);
     }
     
